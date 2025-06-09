819f29aa01441aa35d5543a6577ce8dddcaee473
Christian Nikolov <kristian.b.nikolov.2021@elsys-bg.org>
2025-06-09 15:32:53 +0300

    adding of the vigenere decrypter

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -30,3 +30,25 @@ def vigenere_encrypter(plain_text, key):
 plain_text = "HELLO WORLD"
 cypher = vigenere_encrypter(plain_text, "LEMON")
 print(cypher)
+
+def vigenere_decrypter(plain_text, key):
+    result = []
+    matrix = generate_vigenere_matrix()
+    alphabet = string.ascii_uppercase
+    key = key.upper().replace(" ", "")
+    key_index = 0
+    
+    for ch in plain_text:
+        if ch.upper() in alphabet:
+            row = ord(key[key_index % len(key)]) - ord('A')
+            col = matrix[row].index(ch.upper())
+            decrypted = alphabet[col]
+            result.append(decrypted if ch.isupper() else decrypted.lower())
+            key_index += 1
+        else:
+            result.append(ch)
+    return "".join(result)
+
+cypher = "SIXZB HSDZQ"
+decypher = vigenere_decrypter(cypher, "LEMON")
+print(decypher)

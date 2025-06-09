20f6407725a3a81dfbb17db15e770f6e34644165
Christian Nikolov <kristian.b.nikolov.2021@elsys-bg.org>
2025-06-09 15:21:29 +0300

    vigenere_cypher

---
+++ vigenere_cypher.py
@@ -0,0 +1,32 @@
+import string
+
+def generate_vigenere_matrix():
+    alphabet = list(string.ascii_uppercase)
+    matrix = []
+
+    for i in alphabet:
+        matrix.append("".join(alphabet))
+        alphabet.append(alphabet.pop(0))
+    return matrix
+
+def vigenere_encrypter(plain_text, key):
+    result = []
+    matrix = generate_vigenere_matrix()
+    alphabet = string.ascii_uppercase
+    key = key.upper().replace(" ", "")
+    key_index = 0
+    
+    for ch in plain_text:
+        if ch.upper() in alphabet:
+            row = ord(key[key_index % len(key)]) - ord('A')
+            col = ord(ch.upper()) - ord('A')
+            encrypted = matrix[row][col]
+            result.append(encrypted if ch.isupper() else encrypted.lower())
+            key_index += 1
+        else:
+            result.append(ch)
+    return "".join(result)
+
+plain_text = "HELLO WORLD"
+cypher = vigenere_encrypter(plain_text, "LEMON")
+print(cypher)
